d1a74b913cfb31bde199b7341b47e68ee0ba2c05
Femina PM <64956320+feminapm@users.noreply.github.com>
2023-02-10 23:37:19 +0530

    Update interact.js
    fix alchemy key

--- src/utils/interact.js
+++ src/utils/interact.js
@@ -2,7 +2,7 @@ import {pinJSONToIPFS} from './pinata.js'
 require('dotenv').config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(alchemyKey);
+const web3 = createAlchemyWeb3("wss://eth-mainnet.ws.alchemyapi.io/ws/".concat(alchemyKey));
 
 const contractABI = require('../contract-abi.json')
 const contractAddress = "0x26F8E4cdbeaE4f4BA1cdB95D8fE058807EB6C28b";
